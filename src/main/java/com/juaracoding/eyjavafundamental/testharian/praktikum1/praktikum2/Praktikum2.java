a6b8fc482a0ca588b763214df751edc3649c2946
Enrico Yohanes <plbenrico98@gmail.com>
2023-07-01 16:07:29 +0700

    Praktikum2

---
+++ src/main/java/com/juaracoding/eyjavafundamental/testharian/praktikum1/praktikum2/Praktikum2.java
@@ -0,0 +1,36 @@
+package com.juaracoding.eyjavafundamental.testharian.praktikum1.praktikum2;
+
+import java.util.Scanner;
+
+/*IntelliJ IDEA 2023.1.2 (Community Edition)
+Build #IC-231.9011.34, built on May 16, 2023
+@Author Enrico a.k.a. Enrico Yohanes
+Java Developer
+Created on 01/07/2023 10:14
+@Last Modified 01/07/2023 10:14
+Version 1.0
+*/
+public class Praktikum2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int intInputanAwal = 0;
+        int intTampung = 0;
+
+        System.out.println("Masukkan Angka :");
+
+        while (true) {
+            try {
+                int intBacainputan = scanner.nextInt();
+                intInputanAwal += intBacainputan;
+                intTampung++;
+
+            }catch(Exception e){
+                break;
+            }
+        }
+        double doHitungRataRata = (double) intInputanAwal / intTampung;
+        System.out.println("Hasil rataratanya adalah"+doHitungRataRata);
+    }
+}
+
+
